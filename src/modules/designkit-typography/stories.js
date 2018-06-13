ca669043ecea7d3a51f40c951aee16d5fbedb437
Jason Melgoza <jason@rightscale.com>
2018-06-13 16:21:08 -0700

    Updated stories for typography and utilites

--- src/modules/designkit-typography/stories.js
+++ src/modules/designkit-typography/stories.js
@@ -3,6 +3,14 @@ import { storiesOf } from '@storybook/react';
 import './index.scss';
 
 storiesOf('Typography', module)
-  .add('foo', () => (
-    <div className='foo'>Typography Styles</div>
+  .add('Base', () => (
+    <div>
+      <h1>H1 - The Evil Rabbit Jumped over the Fence</h1>
+      <h2>H2 - The Evil Rabbit Jumped over the Fence</h2>
+      <h3>H3 - The Evil Rabbit Jumped over the Fence</h3>
+      <h4>H4 - The Evil Rabbit Jumped over the Fence</h4>
+      <h5>H5 - The Evil Rabbit Jumped over the Fence</h5>
+      <h6>H6 - The Evil Rabbit Jumped over the Fence</h6>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
   ))
--- src/modules/designkit-utilities/stories.js
+++ src/modules/designkit-utilities/stories.js
@@ -1,8 +1,35 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import './index.scss';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import "./index.scss";
 
-storiesOf('Utilities', module)
-  .add('ba', () => (
-    <div className='ba pa4 shadow'>Basic Box</div>
-  ))
+storiesOf("Utilities", module).add("Borders", () => (
+  <div>
+    <p>Basic Box</p>
+    <div className="ba b-grey shadow pa4">
+      <code>ba b-grey shadow pa4</code>
+    </div>
+    <p>Border Colors</p>
+    <div className="ba b-grey pa4 mb2">
+      <code>ba b-grey</code>
+    </div>
+    <div className="ba b-grey-light pa4">
+      <code>ba b-grey-light</code>
+    </div>
+    <p>Top Border</p>
+    <div className="bt pt3">
+      <code>bt</code>
+    </div>
+    <p>Bottom Border</p>
+    <div className="bb pb3">
+      <code>bb</code>
+    </div>
+    <p>Left Border</p>
+    <span className="bl pl3">
+      <code>bl</code>
+    </span>
+    <p>Right Border</p>
+    <span className="br pr3">
+      <code>br</code>
+    </span>
+  </div>
+));
